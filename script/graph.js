4741a40f3710d40369a056e9ff13cbd4eef9e7f9
Benjamin <benj@zagorski.com>
2017-07-09 10:20:29 -0700

    Reverted back to climate change graph

--- script/graph.js
+++ script/graph.js
@@ -1,54 +1,16 @@
 function makeGraph(data) {
   var dataArray = [
     ["date"].concat(data.dates),
-    ["Susan"].concat(data.minTemps),
-    ["Bob"].concat(data.maxTemps)
+    ["Minimum Temperature"].concat(data.minTemps),
+    ["Maximum Temperature"].concat(data.maxTemps)
   ];
-  var units = "standard";
+  var units = $("#unitsDropdown")[0].value;
   var unitAbrev;
   if (units === "standard") {
     unitAbrev = "F";
   } else if (units === "metric") {
     unitAbrev = "C";
   }
-  var dateLines = [
-    new Date("02/09/17"),
-    new Date("02/30/17"),
-    new Date("03/19/17"),
-    new Date("04/10/17"),
-  ];
-  var notes = [
-    "Added new target",
-    "Decision made: use prompt hierarchy V-G-M.  Use Ipad for first one and for last 9 use non-reinforcer item.  Need to confirm tactic change at next parent-teacher conference.",
-    'Teacher decision: "all" and "on", write down and use red marker for ending sound.',
-    'Added generalization target'
-  ];
-  var proctors = [
-    "Smith, Bob",
-    "Bulgary, Mike",
-    "Delgato, Susan",
-    "Delgato, Ally"
-  ];
-
-  var lineData = [];
-  var legendData = [];
-
-  dateLines.forEach(function(el, ind) {
-    lineData.push(
-      {
-        value: dateLines[ind],
-        text: notes[ind].slice(0, 20) + "..."
-      }
-    );
-    legendData.push(
-      {
-        date: dateLines[ind],
-        text: notes[ind],
-        proctor: proctors[ind]
-      }
-    );
-  });
-
   c3.generate({
     bindto: '#chart',
     data: {
@@ -59,7 +21,7 @@ function makeGraph(data) {
     axis: {
       y: {
         label: {
-          text: 'Percent Correct',
+          text: 'Temperature (' + unitAbrev + ')',
           position: 'outer-middle'
         }
       },
@@ -71,70 +33,6 @@ function makeGraph(data) {
         label: 'Date',
         position: 'outer-middle'
       }
-    },
-    grid: {
-      x: {
-        lines: lineData
-      },
-      // y: {
-      //   lines: [
-      //     {value: 50, text: 'Phase change'}
-      //   ]
-      // },
-    },
-    // onmouseover: function(d) {
-    //   console.log ("yo" + d);
-    // },
-  });
-
-  legendData.forEach(function(legend) {
-    $("#legend").append(
-      "<div class='legendBit hide' id=" + legend.date.toDateString().replace(" ", "").slice(0,6) + "><div id='dateDisplay'>" + legend.date.toDateString() + ": </div>" + legend.text + "<br>- " + legend.proctor + "<br><br></div>"
-    );
-  });
-  addLineHoverFunctionality();
-}
-
-function addLineHoverFunctionality() {
-  $("#chart text").on("mouseenter", function(e) {
-    var dateOfHover = e.target.parentNode.__data__.value;
-    // $("#" + dateOfHover.toDateString().replace(" ", "").slice(0,6)).addClass("bolder");
-    if (dateOfHover) {
-      $("#" + dateOfHover.toDateString().replace(" ", "").slice(0,6)).removeClass("hide");
-    }
-  });
-  $("#chart text").on("mouseleave", function(e) {
-    var dateOfHover = e.target.parentNode.__data__.value;
-    // $("#" + dateOfHover.toDateString().replace(" ", "").slice(0,6)).removeClass("bolder");
-    if (dateOfHover) {
-      $("#" + dateOfHover.toDateString().replace(" ", "").slice(0,6)).addClass("hide");
-    }
-  });
-  $("#chart text").on("mouseenter", function(e) {
-    // console.log(e.target.parentNode.__data__.value);
-    var dateOfHover = e.target.parentNode.__data__.value;
-    // $("#" + dateOfHover.toDateString().replace(" ", "").slice(0,6)).addClass("bolder");
-    if (dateOfHover) {
-      $("#" + dateOfHover.toDateString().replace(" ", "").slice(0,6)).removeClass("hide");
-    }
-  });
-  $("#chart text").on("mouseleave", function(e) {
-    var dateOfHover = e.target.parentNode.__data__.value;
-    // $("#" + dateOfHover.toDateString().replace(" ", "").slice(0,6)).removeClass("bolder");
-    if (dateOfHover) {
-      $("#" + dateOfHover.toDateString().replace(" ", "").slice(0,6)).addClass("hide");
     }
   });
 }
-
-addLineHoverFunctionality();
-
-$("#showAllToggle").on("change", function() {
-  if ($("#showAllToggle")[0].checked) {
-    $(".legendBit").removeClass("hide");
-    $("#chart text").off('mouseenter mouseleave');
-  } else {
-    $(".legendBit").addClass("hide");
-    addLineHoverFunctionality();
-  }
-});
--- script/request.js
+++ script/request.js
@@ -16,13 +16,6 @@ function refreshGraph() {
 }
 
 function makeTempDataRequest(inputData) {
-  // inputData = {
-  //   endDate:"2017-04-30",
-  //   startDate:"2017-01-30",
-  //   stationId:"GHCND:USC00040232",
-  //   unit:"standard",
-  // };
-  // console.log(inputData);
   return new Promise(function(resolve, reject) {
     $.ajax({
       type: "GET",
@@ -49,15 +42,15 @@ function makeTempDataRequest(inputData) {
 
 function getInputs() {
   return {
-      endDate:"2017-04-30",
-      startDate:"2017-01-30",
-      stationId:"GHCND:USC00040232",
-      unit:"standard",
+    startDate: $("#startDate")[0].value,
+    endDate: $("#endDate")[0].value,
+    stationId: $("#stationDropdown")[0].value,
+    unit: $("#unitsDropdown")[0].value,
   };
 }
 
-// $("#startDate")[0].value = ("2017-01-30");
-// $("#endDate")[0].value = ("2017-04-30");
+$("#startDate")[0].value = ("2017-01-30");
+$("#endDate")[0].value = ("2017-04-30");
 
 refreshGraph();
 
